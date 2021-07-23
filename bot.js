5fa29f909e7d01040e06c0149217da855df1f12d
LukeJansen <lpjansen@live.co.uk>
2021-07-24 00:47:08 +0100

    1.1.0
    - Added database features

--- bot.js
+++ bot.js
@@ -3,14 +3,36 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 const Discord = require("discord.js")
+const mongoose = require('mongoose')
 const client = new Discord.Client();
 
+mongoose.connect(process.env.DB_URL, {useNewUrlParser:true, useUnifiedTopology:true})
+const db = mongoose.connection
+db.on('error', (error) => console.log("Database error: " + error))
+db.once('open', () => console.log("Database connected!"))
+
+const userSchema = new mongoose.Schema({
+    userID: {
+        type: String,
+        required: true
+    },
+    channels: {
+        type: Map,
+        required: true
+    },
+    default: {
+        type: String,
+        required: false
+    }
+})
+
+const User = mongoose.model('User', userSchema)
 users = {}
 
-const userTemplate = {
-    channels: {},
-    default: ""
-}
+// const userTemplate = {
+//     channels: {},
+//     default: ""
+// }
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}`)
@@ -26,7 +48,7 @@ client.on('message', async (msg) => {
 
     if (userID == client.user.id) return
 
-    if (!member.voice.channel){
+    if (!member.voice.channel && text.substr(0,4) == "!cn "){
         msg.channel.send("You are not in a voice channel! Please join a channel and then reissue this command").then(function (message) {
             message.react("🗑️")
         }).catch(function() {
@@ -35,34 +57,36 @@ client.on('message', async (msg) => {
         msg.delete()
         return
     }
-
-    const channelID = voiceState.channelID
     
     switch(text.substr(0, 4)){
         case "!cn ":
             try {
-                if (msg.guild.owner.id == msg.author.id) {
+                var user = await User.findOne({userID: member.id})
+                const channelID = voiceState.channelID
+
+                if (msg.guild.ownerID == msg.author.id) {
                     return msg.channel.send("Sorry Server Owners cannot use Channel Nickname Bot :(").then(function (message) {
                         message.react("🗑️")
                     }).catch(function() {
                         console.log("ERROR!")
                     });
                 }
-                if (userID in users){
-                    users[userID].channels[channelID] = text.substr(4)
+
+                if (user != null){
+                    user.channels.set(channelID, text.substr(4))
+                    await user.save()
+
                     member.setNickname(text.substr(4))
                 }
                 else{
-                    users[userID] = {
-                        channels: {},
-                        default: member.displayName
-                    }
+                    var newUser = new User({userID: member.id, channels: {}, default: member.user.username})
+                    newUser.channels.set(channelID, text.substr(4))
+                    await newUser.save()
 
-                    users[userID].channels[channelID] = text.substr(4)
                     member.setNickname(text.substr(4))
                 }
+
                 msg.react("✅").then(() => msg.react('🗑️'))
-                console.log(users)
             }
             catch (error){
                 console.error(error)
@@ -73,28 +97,37 @@ client.on('message', async (msg) => {
                 });
             }
             break
+
         case "!cnr":
-            member.setNickname(users[userID].default)
-            delete users[userID]
+            
+            await User.deleteOne({userID: member.id})
+            
+            member.setNickname("")
             msg.react("✅").then(() => msg.react('🗑️'))
             break
+
         case "!cnd":
-            if (msg.guild.owner.id == msg.author.id) {
+            var user = await User.findOne({userID: member.id})
+            
+            if (msg.guild.ownerID == msg.author.id) {
                 return msg.channel.send("Sorry Server Owners cannot use Channel Nickname Bot :(").then(function (message) {
                     message.react("🗑️")
                 }).catch(function() {
                     console.log("ERROR!")
                 });
             }
-            if (userID in users){
-                users[userID].default = text.substr(5)
+
+            if (user != null){
+                user.default = text.substr(4)
+                await user.save()
             }
             else{
-                users[userID] = {
-                    channels: {},
-                    default: text.substr(5)
-                }
+                user = new User({userID: member.id, channels: {}, default: text.substr(4)})
+                await user.save()
             }
+
+            if (voiceState == null) member.setNickname(text.substr(4))
+
             msg.react("✅").then(() => msg.react('🗑️'))
             break
         case "!cn?":
@@ -108,23 +141,24 @@ client.on('message', async (msg) => {
     }
 })
 
-client.on("voiceStateUpdate", function(oldMember, newMember){
+client.on("voiceStateUpdate", async function(oldMember, newMember){
 
-    const userID = newMember.member.user.id
-    if (userID in users){
-        console.log(newMember.channelID)
+    const userID = newMember.member.id
+    const user = await User.findOne({userID: userID})
+
+    if (user != null){
         
         if (newMember.channelID == null){
-            newMember.member.setNickname(users[userID].default)
+            newMember.member.setNickname(user.default)
             return
         }
         const channelID = newMember.channel.id
 
-        if (channelID in users[userID].channels){
-            newMember.member.setNickname(users[userID].channels[channelID])
+        if (user.channels.get(channelID) != null){
+            newMember.member.setNickname(user.channels.get(channelID))
         }
         else{
-            newMember.member.setNickname(users[userID].default)
+            newMember.member.setNickname(user.default)
         }
     }
 });
@@ -142,15 +176,11 @@ client.on('messageReactionAdd', async (reaction, user) => {
 			return;
 		}
     }
-    
-    console.log(reaction.count)
 
     if (reaction.users.cache.has(client.user.id) && reaction._emoji.name == "🗑️"){
-        console.log("Delete 2")
         if (reaction.count >= 2) reaction.message.delete()
     }
     else if (reaction.users.cache.has(reaction.message.author.id) && reaction._emoji.name == "🗑️"){
-        console.log("Delete 1")
         reaction.message.delete()
     }
 });
