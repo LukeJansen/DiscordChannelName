dc52574e03d8428d7424a4985a65da3e0d9997ab
LukeJansen <lpjansen@live.co.uk>
2022-10-08 16:05:42 +0100

    V1.4
    - Updated for new discord slash commands
    - Updated for new discord.js
    - Updated logging for clarity
    - Moved slash command generation to separate file to be ran on its own when needed.

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config()
 }
 
-const {Client, Intent, Intents, MessageActionRow, MessageButton} = require("discord.js")
+const {Client, GatewayIntentBits, ActionRowBuilder, ButtonBuilder, ButtonStyle} = require("discord.js")
 const mongoose = require('mongoose')
 
 
@@ -28,87 +28,26 @@ const userSchema = new mongoose.Schema({
 
 const User = mongoose.model('User', userSchema)
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_VOICE_STATES, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_PRESENCES] });
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildVoiceStates, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildPresences] });
 
-const commands = [
-    {
-        name:'cn',
-        description: 'Set nickname for current voice channel',
-        options: [
-            {
-                name: "nickname",
-                description: "The nickname for this channel",
-                type: 'STRING',
-                required: true
-            }
-        ]
-    },
-    {
-        name:'cndefault',
-        description: 'Set default nickname',
-        options: [
-            {
-                name: "nickname",
-                description: "The default nickname for you",
-                type: 'STRING',
-                required: true
-            }
-        ]
-    },
-    {
-        name:'cnreset',
-        description: 'Reset all your nicknames (PROD)'
-    },
-    {
-        name:'cnhelp',
-        description: 'View all commmands for Channel Nickname Bot'
-    },
-    {
-        name:'gank',
-        description: 'Moan at someone in particular',
-        options: [
-            {
-                name: "jungler",
-                description: "The jungler who did not gank you",
-                type: 'USER',
-                required: true
-            }
-        ]
-    },
-    {
-        name:'egirl',
-        description: 'Moan at daddy',
-        options: [
-            {
-                name: "daddy",
-                description: "The daddy you want to nya at",
-                type: 'USER',
-                required: true
-            }
-        ]
-    }
-]
-
-function logVC(channelID, userID, nickname){
-    console.info(`[INFO] ${userID} joined voice channel ${channelID} and was assigned nickname ${nickname}.`)
+function logVC(channelID, channelName, userID, userName, nickname){
+    console.info(`[INFO] ${userID} (${userName}) joined voice channel ${channelID} (${channelName}) and was assigned nickname '${nickname}'.`)
 }
 
-function logCN(channelID, userID, nickname){
-    console.info(`[INFO] ${userID} asssigned nickname ${nickname} to ${channelID}.`)
+function logCN(channelID, channelName, userID, userName, nickname){
+    console.info(`[INFO] ${userID} (${userName}) asssigned nickname '${nickname}' to ${channelID} (${channelName}).`)
 }
 
-function logCND(userID, nickname){
-    console.info(`[INFO] ${userID} asssigned default nickname ${nickname}.`)
+function logCND(userID, userName, nickname){
+    console.info(`[INFO] ${userID} (${userName}) asssigned default nickname '${nickname}'.`)
 }
 
-function logCNR(userID, nickname){
-    console.info(`[INFO] ${userID} reset all nicknames.`)
+function logCNR(userID, userName){
+    console.info(`[INFO] ${userID} (${userName}) reset all nicknames.`)
 }
 
 client.on('ready', async () => {
     console.log(`Logged in as ${client.user.tag}\n`)
-
-    await client.application?.commands.set(commands)
 
     client.user.setUsername("Channel Nickname Bot")
     client.user.setStatus("online")
@@ -121,11 +60,12 @@ client.on('ready', async () => {
 // Slash Command Interactions
 client.on('interactionCreate', async (interaction) => {
 
-    if (!interaction.isCommand()) return;
+    if (!interaction.isChatInputCommand()) return;
 
     const nickname = interaction.options.getString('nickname')
     const member = interaction.member
     const userID = interaction.user.id
+    const userName = interaction.member.user.tag
     const ownerID = member.guild.ownerId
     const voiceState = member.guild.voiceStates.cache.get(interaction.user.id)
 
@@ -133,11 +73,11 @@ client.on('interactionCreate', async (interaction) => {
         case "cn":
 
             if (nickname == null || nickname == "" || nickname.length < 2 || nickname.length > 32){
-                await interaction.reply({ content: "Sorry! Nicknames must be between 2 and 32 characters long. ❌", ephemeral: true })
+                await interaction.reply({ content: ":x: Sorry! Nicknames must be between 2 and 32 characters long. :x:", ephemeral: true })
                 return
             }
             else if (ownerID == userID) {
-                await interaction.reply({ content: "Sorry! Server owners cannot yet use this bot 😭", ephemeral: true })
+                await interaction.reply({ content: ":cry: Sorry! Server owners cannot use this bot :cry:", ephemeral: true })
                 return
             }
 
@@ -145,17 +85,18 @@ client.on('interactionCreate', async (interaction) => {
                 var user = await User.findOne({userID: userID})
 
                 if (voiceState == null){
-                    await interaction.reply({ content: "You must be in a voice channel to use this command. ❌", ephemeral: true })
+                    await interaction.reply({ content: ":x: You must be in a voice channel to use this command. :x:", ephemeral: true })
                     return
                 }
 
-                const channelID = voiceState.channelId            
+                const channelID = voiceState.channelId  
+                const channelName = voiceState.channel.name          
 
                 if (user != null){
                     user.channels.set(channelID, nickname)
                     await user.save()
                     
-                    logCN(channelID, userID, nickname)
+                    logCN(channelID, channelName, userID, userName, nickname)
                     member.setNickname(nickname)
                 }
                 else{
@@ -163,30 +104,31 @@ client.on('interactionCreate', async (interaction) => {
                     newUser.channels.set(channelID, nickname)
                     await newUser.save()
                     
-                    logCN(channelID, userID, nickname)
+                    logCN(channelID, channelName, userID, userName, nickname)
                     member.setNickname(nickname)
                 }
 
-                await interaction.reply({ content: "Nickname Set! :white_check_mark:", ephemeral: true })
+                await interaction.reply({ content: ":white_check_mark: Nickname Set! :white_check_mark:", ephemeral: true })
             }
             catch (error){
                 console.error(error)
-                await interaction.reply({ content: "Something went wrong! Oops! Get <@275348412797550595> to have a look.", ephemeral: false })
+                await interaction.reply({ content: ":x: Something went wrong! Oops! Get <@275348412797550595> to have a look. :x:", ephemeral: false })
             }
             break
 
         case "cnreset":
 
-            const row = new MessageActionRow().addComponents(
-                new MessageButton()
-                    .setCustomId('resetYes')
-                    .setLabel('Yes')
-                    .setStyle('SUCCESS'),
-                new MessageButton()
-                    .setCustomId('resetNo')
-                    .setLabel('No')
-                    .setStyle('DANGER')
-            )
+            const row = new ActionRowBuilder()
+                .addComponents(
+                    new ButtonBuilder()
+                        .setCustomId('resetYes')
+                        .setLabel('Yes')
+                        .setStyle(ButtonStyle.Success),
+                        new ButtonBuilder()
+                        .setCustomId('resetNo')
+                        .setLabel('No')
+                        .setStyle(ButtonStyle.Danger)
+                )
 
             await interaction.reply( {content: 'Are you sure you want to reset all nicknames?', components: [row], ephemeral: true})
         
@@ -195,11 +137,11 @@ client.on('interactionCreate', async (interaction) => {
         case "cndefault":
 
             if (nickname == null || nickname == "" || nickname.length < 2 || nickname.length > 32){
-                await interaction.reply({ content: "Sorry! Nicknames must be between 2 and 32 characters long. ❌", ephemeral: true })
+                await interaction.reply({ content: ":x: Sorry! Nicknames must be between 2 and 32 characters long. :x:", ephemeral: true })
                 return
             }
             else if (ownerID == userID) {
-                await interaction.reply({ content: "Sorry! Server owners cannot yet use this bot 😭", ephemeral: true })
+                await interaction.reply({ content: ":cry: Sorry! Server owners cannot yet use this bot :cry:", ephemeral: true })
                 return
             }
 
@@ -208,20 +150,20 @@ client.on('interactionCreate', async (interaction) => {
             if (user != null){
                 user.default = nickname
                 await user.save()
-                logCND(userID, nickname)
+                logCND(userID, userName, nickname)
             }
             else{
                 user = new User({userID: userID, channels: {}, default: nickname})
                 await user.save()
-                logCND(userID, nickname)
+                logCND(userID, userName, nickname)
             }
 
             if (voiceState == null) member.setNickname(nickname)
 
-            await interaction.reply({ content: "Default Nickname Set! :white_check_mark:", ephemeral: true })
+            await interaction.reply({ content: ":white_check_mark: Default Nickname Set! :white_check_mark:", ephemeral: true })
             break
         case "cnhelp":
-            await interaction.reply({ content: "❔ Channel Nickname Bot Help! ❔ \n/cn {nickname}- Set nickname for current voice channel. \n/cndefault {nickname} - Set default nickname for when you are not in a nicknamed channel. \n/cnreset - Reset all stored nicknames for yourself. \n/cnhelp - View this help message.\n\n:tada: Meme Commands :tada:\n/gank {jungler} - Moan at the jungler who didn't gank you!\n/egirl {daddy} - Moan at dadddy", ephemeral: true })
+            await interaction.reply({ content: ":grey_question: Channel Nickname Bot Help! :grey_question: \n/cn {nickname}- Set nickname for current voice channel. \n/cndefault {nickname} - Set default nickname for when you are not in a nicknamed channel. \n/cnreset - Reset all stored nicknames for yourself. \n/cnhelp - View this help message.\n\n:tada: Meme Commands :tada:\n/gank {jungler} - Moan at the jungler who didn't gank you!\n/egirl {daddy} - Moan at dadddy", ephemeral: true })
             break
 
         case "gank":
@@ -241,6 +183,7 @@ client.on('interactionCreate', async (interaction) => {
 	if (!interaction.isButton()) return;
 
     const userID = interaction.user.id
+    const userName = interaction.member.user.tag
     const ownerID = interaction.member.guild.ownerID
 	
     switch(interaction.customId){
@@ -250,13 +193,13 @@ client.on('interactionCreate', async (interaction) => {
             
             if (userID != ownerID) interaction.member.setNickname("")
 
-            logCNR(userID)
+            logCNR(userID, userName)
 
-            await interaction.update( {content: "All nicknames have been reset! :white_check_mark:", components: [], ephemeral: true})
+            await interaction.update( {content: "Your nicknames have been reset! :white_check_mark:", components: [], ephemeral: true})
             break
 
         case "resetNo":
-            await interaction.update( {content: "No nicknames have been reset! ❌", components: [], ephemeral: true})
+            await interaction.update( {content: "Your nicknames have not been reset! :negative_squared_cross_mark:", components: [], ephemeral: true})
             break
     }
 
@@ -266,6 +209,7 @@ client.on('interactionCreate', async (interaction) => {
 client.on("voiceStateUpdate", async function(oldMember, newMember){
 
     const userID = newMember.member.id
+    const userName = newMember.member.user.tag
     const channelID = newMember.channelId
     const user = await User.findOne({userID: userID})
 
@@ -273,18 +217,20 @@ client.on("voiceStateUpdate", async function(oldMember, newMember){
         
         if (channelID == null){
             newMember.member.setNickname(user.default)
-            logVC("DEFAULT", userID, user.default)
+            //channelID, channelName, userID, userName, nickname
+            logVC("DEFAULT", "DEFAULT", userID, userName, user.default)
             return
         }
         
+        const channelName = newMember.channel.name
 
         if (user.channels.get(channelID) != null){
             newMember.member.setNickname(user.channels.get(channelID))
-            logVC(channelID, userID, user.channels.get(channelID))
+            logVC(channelID, channelName, userID, userName, user.channels.get(channelID))
         }
         else{
             newMember.member.setNickname(user.default)
-            logVC(channelID, userID, user.default)
+            logVC(channelID, channelName, userID, userName, user.default)
         }
     }
 });
---
+++ deploy-commands.js
@@ -0,0 +1,49 @@
+const { REST, SlashCommandBuilder, Routes } = require('discord.js');
+//const { clientId, guildId, token } = require('./config.json');
+require('dotenv').config();
+const token = process.env.BOT_TOKEN;
+const client_id = process.env.CLIENT_ID;
+
+const commands = [
+	new SlashCommandBuilder()
+        .setName('cn')
+        .setDescription('Set nickname for current voice channel')
+        .addStringOption(option => 
+            option.setName("nickname")
+                .setDescription("The nickname for your currently connected voice channel")
+                .setRequired(true)),
+    new SlashCommandBuilder()
+        .setName('cndefault')
+        .setDescription('Set default nickname')
+        .addStringOption(option => 
+            option.setName("nickname")
+                .setDescription("The default nickname for when you are not connected to a voice channel")
+                .setRequired(true)),
+    new SlashCommandBuilder()
+        .setName('cnreset')
+        .setDescription('Reset all your nicknames'),
+    new SlashCommandBuilder()
+        .setName('cnhelp')
+        .setDescription('View all commmands for Channel Nickname Bot'),
+    new SlashCommandBuilder()
+        .setName('gank')
+        .setDescription('Moan at someone in particular')
+        .addUserOption(option => 
+            option.setName("jungler")
+                .setDescription("The jungler who did not gank you")
+                .setRequired(true)),
+    new SlashCommandBuilder()
+        .setName('egirl')
+        .setDescription('Moan at daddy')	
+        .addUserOption(option => 
+            option.setName("daddy")
+                .setDescription("The daddy you want to 'nya' at")
+                .setRequired(true))
+]
+	.map(command => command.toJSON());
+
+const rest = new REST({ version: '10' }).setToken(token);
+
+rest.put(Routes.applicationCommands(client_id), { body: commands })
+	.then((data) => console.log(`Successfully registered ${data.length} application commands.`))
+	.catch(console.error);
